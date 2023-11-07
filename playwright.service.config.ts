4ff57d9022698f0b9f5cae3b3fd025ee46e9c9e2
John Stallo <john.stallo@microsoft.com>
2023-11-07 17:28:30 +0800

    first commit

---
+++ playwright.service.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from '@playwright/test';
+import config from './playwright.config';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Name the test run if it's not named yet.
+process.env.PLAYWRIGHT_SERVICE_RUN_ID = process.env.PLAYWRIGHT_SERVICE_RUN_ID || new Date().toISOString();
+
+// Can be 'linux' or 'windows'.
+const os = process.env.PLAYWRIGHT_SERVICE_OS || 'linux';
+
+export default defineConfig(config, {
+  workers: 20,
+  ignoreSnapshots: false,
+  snapshotPathTemplate: `{testDir}/__screenshots__/{testFilePath}/${os}/{arg}{ext}`,
+  
+  use: {
+    // Specify the service endpoint.
+    connectOptions: {
+      wsEndpoint: `${process.env.PLAYWRIGHT_SERVICE_URL}?cap=${JSON.stringify({
+        os,
+        runId: process.env.PLAYWRIGHT_SERVICE_RUN_ID
+      })}`,
+      timeout: 30000,
+      headers: {
+        'x-mpt-access-key': process.env.PLAYWRIGHT_SERVICE_ACCESS_TOKEN!
+      },
+      // Allow service to access the localhost.
+      exposeNetwork: '<loopback>'
+    }
+  }
+});

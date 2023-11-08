2ff15623d491787119965abd26d609d19e37bdd0
John Stallo <john.stallo@microsoft.com>
2023-11-08 12:48:17 +0800

    add tests

---
+++ tests/user-reviews.spec.ts
@@ -0,0 +1,15 @@
+import fs from 'fs';
+import { test, expect } from '@playwright/test';
+
+const testFilename = 'user-reviews';
+const testNames = fs.readFileSync('data/' + testFilename + '.txt', 'utf-8').split('\n').map(name => name.trim()).filter(Boolean);
+
+test.describe(testFilename, () => {
+  for (const testName of testNames) {
+    test(testName, async ({ page }) => {
+      await page.goto('https://playwright.dev/');
+      await page.getByRole('link', { name: 'Get started' }).click();
+      await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    });
+  }
+});
